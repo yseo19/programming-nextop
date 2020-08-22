8f44797195ac68db3c1209dce0c5b75b27e8fd7d
yseo19 <67813798+yseo19@users.noreply.github.com>
2020-08-22 17:59:39 +0900

    Add files via upload

--- E3/e3_숫자야구.cpp
+++ E3/e3_숫자야구.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 struct baseball {
-	int n;
+	int num;
 	int str;
 	int ball;
 };
@@ -13,49 +13,75 @@ int main(void) {
 	ifstream fin("input.txt");
 
 	int n;
-	int i;
+	int i, j;
 	int k = 0;
-	int a;
 	baseball A[100];
 	int cnt[2] = { 0, };
 
 	fin >> n;
 
-	for (i = 0; i < n; i++) 
-		fin >> A[i].n >> A[i].str >> A[i].ball;
+	for (i = 0; i < n; i++)
+		fin >> A[i].num >> A[i].str >> A[i].ball;
 
-	a = 110;
-	
-	while (1) {
-		a++;
-		cnt[0] = 0;
-		cnt[1] = 0;
+	for (i = 123; i <= 987; i++) {
+		for (j = 0; j < n; j++) {
+			cnt[0] = 0;
+			cnt[1] = 0;
 
-		for (i = 0; i < n; i++) {
-			if (a / 100 == A[i].n / 100)
+			if (i / 100 == A[j].num / 100)
 				cnt[0]++;
-			if (a / 100 == (A[i].n / 10) % 10 || a / 100 == A[i].n % 10)
+			if (i / 100 == (A[j].num / 10) % 10 || i / 100 == A[j].num % 10)
 				cnt[1]++;
-			if ((a / 10) % 10 == (A[i].n / 10) % 10)
+			if ((i / 10) % 10 == (A[j].num / 10) % 10)
 				cnt[0]++;
-			if ((a / 10) % 10 == A[i].n/100 || (a/10)%10 == A[i].n % 10)
+			if ((i / 10) % 10 == A[j].num / 100 || (i / 10) % 10 == A[j].num % 10)
 				cnt[1]++;
-			if (a % 10 == A[i].n % 10)
+			if (i % 10 == A[j].num % 10)
 				cnt[0]++;
-			if (a % 10 == (A[i].n / 10) % 10 || a % 10 == A[i].n / 100)
+			if (i % 10 == (A[j].num / 10) % 10 || i % 10 == A[j].num / 100)
 				cnt[1]++;
 
-			if (cnt[0] != A[i].str || cnt[1] != A[i].ball)
-				continue;
+			if (cnt[0] != A[j].str || cnt[1] != A[j].ball)
+				break;
 		}
+		if (j == n - 1)
+			k++;
 
-		k++;
-
-		if (a >= 999)
-			break;
+		
 	}
 
 
+
+	/*a = 110;
+
+	a++;
+	cnt[0] = 0;
+	cnt[1] = 0;
+
+	for (i = 0; i < n; i++) {
+		if (a / 100 == A[i].n / 100)
+			cnt[0]++;
+		if (a / 100 == (A[i].n / 10) % 10 || a / 100 == A[i].n % 10)
+			cnt[1]++;
+		if ((a / 10) % 10 == (A[i].n / 10) % 10)
+			cnt[0]++;
+		if ((a / 10) % 10 == A[i].n / 100 || (a / 10) % 10 == A[i].n % 10)
+			cnt[1]++;
+		if (a % 10 == A[i].n % 10)
+			cnt[0]++;
+		if (a % 10 == (A[i].n / 10) % 10 || a % 10 == A[i].n / 100)
+			cnt[1]++;
+
+		if (cnt[0] != A[i].str || cnt[1] != A[i].ball)
+			continue;
+	}
+
+	k++;
+
+	if (a >= 999)
+		break;
+
+	*/
 	printf("%d", k);
 
 
