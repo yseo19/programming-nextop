99b6357a862c098049d77ee1958a98ec37da3d6c
yseo19 <67813798+yseo19@users.noreply.github.com>
2020-09-19 12:55:21 +0900

    Add files via upload

--- E3/e3_보물섬.cpp
+++ E3/e3_보물섬.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #define N 50
-int nx, ny;
+int nx, ny, MAX;
 char A[N][N];
 int B[N][N], Q[N*N][3];
 int c[] = { -1,0,1,0 };
@@ -16,23 +16,49 @@ int check(int a, int b) {
 	else return 1;
 }
 
+void cls() {
+	for (int i = 0; i < ny; i++) {
+		for (int j = 0; j < nx; j++)
+			B[i][j] = 0;
+	}
+}
 
-void BFS() {
-	int i;
+
+void BFS(int k1, int k2) {
+	int i, x, y, front = 0, rear = 1;
 	char temp;
 	int temp_2, temp_3;
+	Q[rear][0] = k1;
+	Q[rear][1] = k2;
+	Q[rear][2] = 1;
+	B[k1][k2] = 1;
 
-	for (i = 0; i < 4; i++) {
-		temp = A[y + c[i]][x + d[i]];
-		temp_2 = check(y + c[i], x + d[i]);
-		temp_3 = B[y + c[i]][x + d[i]];
-		if (temp == 'L' && temp_2 && !temp_3) {
-			rear++;
-			Q[rear][0] = y + c[i];
-			Q[rear][1] = x + d[i];
-			Q[rear][2] = B[y][x] + 1
+
+
+	while (1) {
+		if (rear == front)
+			break;
+
+		front++;
+		y = Q[front][0];
+		x = Q[front][1];
+
+
+		for (i = 0; i < 4; i++) {
+			temp = A[y + c[i]][x + d[i]];
+			temp_2 = check(y + c[i], x + d[i]);
+			temp_3 = B[y + c[i]][x + d[i]];
+			if (temp == 'L' && temp_2 && !temp_3) {
+				rear++;
+				Q[rear][0] = y + c[i];
+				Q[rear][1] = x + d[i];
+				Q[rear][2] = Q[front][2] + 1;
+				B[y + c[i]][x + d[i]] = B[y][x] + 1;
+			}
 		}
 	}
+	if (MAX < Q[front][2])
+		MAX = Q[front][2];
 		
 
 
@@ -53,10 +79,10 @@ int main(void) {
 	for (i = 0; i < ny; i++) {
 		for (j = 0; j < nx; j++) {
 			if (A[i][j] == 'L')
-				BFS();
+				BFS(i, j);
 		}
 	}
-
+	printf("%d", MAX);
 
 
 
